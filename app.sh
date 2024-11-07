43e0967c6387a3da6dc428b19655bd5eb7f5384c
LeVzhe <levzh93@gmail.com>
2024-11-07 18:50:46 +0300

    first commit

---
+++ app.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+case "$1" in
+# Запуск контейнера
+    up)
+        echo "Запуск Docker Compose..."
+        docker compose up -d
+        ;;
+# Остановка и удаление контейнеров
+    down)
+        echo "Остановка Docker Compose..."
+        docker compose down
+        ;;
+    build)
+        echo "Остановка Docker Compose..."
+        docker compose build
+        ;;
+#------------------------------------------------#
+# manage.py команды
+    makemigrations)
+        echo "Создание миграций..."
+        docker compose exec backend-django python manage.py makemigrations
+        ;;
+    migrate)
+        echo "Мигрирование..."
+        docker compose exec backend-django python manage.py migrate
+        ;;
+    createsuperuser)
+        echo "Создание супер-пользователя..."
+        docker compose exec backend-django python manage.py createsuperuser
+        ;;
+    startapp)
+        if [ -z "$2" ]; then
+            echo "Ошибка: Укажите имя приложения."
+            exit 1
+        fi
+        APP_NAME="$2"
+        echo "Создание приложения '$APP_NAME'..."
+        cd backend_books_review
+        django-admin startapp "$APP_NAME"
+        ;;
+#------------------------------------------------#
+# остальные команды
+    freeze)
+        echo "Обновление зависимостей..."
+        pip freeze > backend_books_review/requirements.txt
+        ;;
+    isort)
+        echo "Запуск isort..."
+        isort ./backend_books_review
+        ;;
+    *)
+        echo "Данной команды не существует."
+        exit 1
+        ;;
+esac
