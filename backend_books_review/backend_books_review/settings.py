be92fa1e57ebaf666a201e94bf339b759afdcc78
LeVzhe <levzh93@gmail.com>
2024-11-13 19:56:14 +0300

    feat: add reviews app, create Review model

--- backend_books_review/backend_books_review/settings.py
+++ backend_books_review/backend_books_review/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     "django.contrib.postgres",
     "users",
     "books",
+    "reviews",
     # "dj_rest_auth",
     "django_cleanup.apps.CleanupConfig",
 ]
@@ -168,7 +169,7 @@ REST_FRAMEWORK = {
 
 # DRF JWT
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
---
+++ backend_books_review/reviews/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+from books.models import Book
+from users.models import User
+
+
+class Review(models.Model):
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+        verbose_name="Книга",
+    )
+    post_owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name="Автор публикации",
+    )
+    content = models.CharField(
+        max_length=256,
+        verbose_name="Отзыв",
+    )
+    publicated = models.DateTimeField(
+        auto_now_add=True,
+        db_index=True,
+        verbose_name="Опубликовано",
+    )
+    rating = models.SmallIntegerField(
+        max_length=5,
+        help_text="Оценка книги от 0 до 5",
+        blank=True,
+        null=True,
+        verbose_name="Оценка",
+    )
